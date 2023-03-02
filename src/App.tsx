251928b130bf9efb0d293d527b35e152f3f13b68
mohamed salamat <moahmed.salamat@gmail.com>
2023-03-02 17:48:55 +0000

    User story 1 requirements: TodoList component to display completed and pending tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import TodoList from './components/TodoList'
+import TodoList, {TaskTypes} from './components/TodoList'
+
+const Tasks = [{
+  id: 't1',
+  description: 'ttoo',
+  type: TaskTypes.PENDING
+},
+{
+  id: 't2',
+  description: 'tt44',
+  type: TaskTypes.COMPLETED
+}]
 
 function App() {
-  return (<TodoList tasks={['yoyo']}/>);
+  return (<TodoList tasks={Tasks}/>);
 }
 
 export default App;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,50 @@
 
+export enum TaskTypes {
+    COMPLETED='completed',
+    PENDING='pending'
+} 
 
-
-type TodoListProps = {
-    tasks: string[]
+type TaskType = {
+    id: string,
+    description: string,
+    type: TaskTypes
 }
 
+type TodoListProps = {
+    tasks: TaskType[]
+}
+
+type TaskProps= {
+    task: TaskType
+}
+
+const Task = ({task}: TaskProps) => {
+    const isCompleted = task.type === TaskTypes.COMPLETED
+    return (
+        <div key={task.id} style={{textDecoration: 'line-through'}} data-testid="task">
+            <input type="checkbox" checked={isCompleted}  onChange={()=>{}} id={task.id} /> <label htmlFor={task.id}>{task.description} </label>
+         </div>
+    )
+}
 const TodoList = ({tasks}: TodoListProps) => {
-    return (<> hello todo list </>)
+    const pendingTasks: JSX.Element[] = []
+    const completedTasks: JSX.Element[] = []
+    
+    tasks.forEach((task) => {
+        if( task.type === TaskTypes.PENDING) {
+            pendingTasks.push(<Task key={task.id} task={task}/>)
+            
+            return
+        }
+
+
+        completedTasks.push(<Task key={task.id} task={task}/>)
+    })
+
+    return (<> 
+        {pendingTasks}
+    {completedTasks}
+     </>)
 }
 
 

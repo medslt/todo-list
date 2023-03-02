6abc30b61d89a83c10bf0db508f6c99f89d771fc
mohamed salamat <moahmed.salamat@gmail.com>
2023-03-02 19:19:06 +0000

    User story 3 unit testing : user should be able to change the status of existing task

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within, waitFor } from '@testing-library/react';
 import userEvent from "@testing-library/user-event"
 
 import App from '../App';
 
 
 
-test('should be able to create a task', () => {
+test('User story 2: should be able to create a task', () => {
   render(<App />);
 
   const taskDescription = 'new Task Description'
@@ -23,3 +23,54 @@ test('should be able to create a task', () => {
 
   expect(screen.getByText(/new Task Description/i)).toBeInTheDocument();
 });
+
+
+test('User story 3: should be able to change the status of existing tasks', async () => {
+    render(<App />);
+  
+    const taskDescription = 'new Task Description'
+
+    const  createTaskEl = screen.getByRole('textbox', {
+      name: /enter your new task:/i
+    });
+  
+    // create new task 
+    const addTaskBtn = screen.getByRole('button', {
+      name: /add task/i
+    })
+
+    userEvent.type(createTaskEl, taskDescription)
+    userEvent.click(addTaskBtn)
+
+
+    const pendingListEl = screen.getByTestId('pendingTasks')
+
+    const completedListEl = screen.getByTestId('completedTasks')
+
+    // task should be added to the pending list by default
+    let taskEl = within(pendingListEl).getByText(/new Task Description/i)
+    
+    // task should be  added to the pending list by default
+    expect(taskEl).toBeInTheDocument()
+
+    // Should be able to move the task to the completed list
+    const checkboxEl =  within(pendingListEl).getByRole('checkbox', {
+        name: /new Task Description/i
+      })
+
+    userEvent.click(checkboxEl)
+
+     taskEl = within(completedListEl).getByText(/new Task Description/i)
+
+
+     expect(taskEl).toBeInTheDocument()
+
+
+     // Should be able to move back the task to the pending list
+    userEvent.click(checkboxEl)
+
+     taskEl = within(completedListEl).getByText(/new Task Description/i)
+
+     expect(taskEl).toBeInTheDocument()
+  });
+  
